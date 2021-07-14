8462557e33a1cb0abb1dae55bb0b2c7c7c839ef3
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-07-14 16:22:21 -0300

    script

---
+++ mobile-navbar.js
@@ -0,0 +1,16 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu)
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+    }
+
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log
+        ("Hey man!"));
+     }
+}
+
+
