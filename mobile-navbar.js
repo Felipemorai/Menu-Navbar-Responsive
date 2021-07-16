dc62e3ab7e64c4fb05cd0c97b80772110176d295
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-07-16 14:57:58 -0300

    commit

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -11,6 +11,19 @@ class MobileNavbar {
         this.mobileMenu.addEventListener("click", () => console.log
         ("Hey man!"));
      }
+
+     init() {
+         if (this.mobileMenu) {
+             this.addClickEvent();
+         }
+         return this;
+
+     }
 }
 
-
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
